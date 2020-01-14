d74a1ffa91473303701bde4ee56c232e37761130
Michael Sarpong <mike.sarp.ky@gmail.com>
2020-01-14 17:18:11 +0000

    a task can now be posted and also text field is partially validated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
-var addTaskButton = document.getElementById("add-task-button");
-var taskForm = document.getElementById("task-form");
+{
+  var addTaskButton = document.getElementById("add-task-button");
+  var taskForm = document.getElementById("task-form");
 
-addTaskButton.onclick = addTask;
+  addTaskButton.onclick = addTask;
 
-function addTask(e) {
+  function addTask(e) {
     e.preventDefault();
-    var newTask = document.getElementById("new-task").value;
-    console.log(newTask);
-    taskForm.reset();
+    var task = document.getElementById("new-task").value;
+
+    if (task === "") {
+      var validationMessage = document.getElementById("validation-message");
+      validationMessage.style.display = "block";
+    } else {
+      var p = document.createElement("p");
+      var textContent = document.createTextNode(task);
+      p.appendChild(textContent);
+      p.classList.add("list-of-task");
+      document.getElementById("display-task").appendChild(p);
+      //   console.log(p);
+      taskForm.reset();
+    }
+  }
 }
