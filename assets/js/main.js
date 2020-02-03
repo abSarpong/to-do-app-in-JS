ea17521d85a4f86ea85bc2bb46b88a17828efa15
Michael Sarpong <mike.sarp.ky@gmail.com>
2020-02-03 20:24:27 +0000

    saving changes made

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,51 @@
 {
-  var addTaskButton = document.getElementById("add-task-button");
-  var taskForm = document.getElementById("task-form");
+    var addTaskButton = document.getElementById("add-task-button");
+    var taskForm = document.getElementById("task-form");
 
-  var div;
-  var task;
-  var icon;
+    var div;
+    var task;
+    var icon;
+    var counter = 0;
 
-  addTaskButton.onclick = addTask;
+    addTaskButton.onclick = addTask;
 
-  function addTask(e) {
-    e.preventDefault();
-    var inputTask = document.getElementById("new-task").value;
+    function addTask(e) {
+        e.preventDefault();
+        var inputTask = document.getElementById("new-task").value;
 
-    if (inputTask === "") {
-      var validationMessage = document.getElementById("validation-message");
-      validationMessage.style.display = "block";
-    } else {
-      div = document.createElement("div");
-      task = document.createElement("p");
-      icon = document.createElement("i");
-      var textContent = document.createTextNode(inputTask);
+        if (inputTask === "") {
+            var validationMessage = document.getElementById("validation-message");
+            validationMessage.style.display = "block";
+        } else {
+            var delEle = document.createElement('eleTag');
+            delEle.setAttribute('id', counter);
+            var newEle = document.getElementById(counter);
+            console.log(newEle);
+            // counter++;
+            div = document.createElement("div");
+            task = document.createElement("p");
+            icon = document.createElement("i");
+            var textContent = document.createTextNode(inputTask);
 
-      icon.classList.add("fas", "fa-times-circle");
-      task.appendChild(textContent);
-      div.append(task, icon);
-      div.classList.add("list-of-task");
-      document.getElementById("display-task").appendChild(div);
+            icon.classList.add("fas", "fa-times-circle");
+            task.appendChild(textContent);
+            div.append(task, icon, counter);
+            div.classList.add("list-of-task");
+            document.getElementById("display-task").appendChild(div);
 
-      // console.log(div);
-      taskForm.reset();
+            console.log(div);
+            taskForm.reset();
+        }
+        icon.onclick = function deleteTask() {
+            // alert("clicked");
+            // uId();
+            div.parentNode.removeChild(div);
+        }
     }
-  }
+
+    // function uId() {
+    //     // counter = counter + 1;
+    //     // console.log(counter);
+    //     div.parentNode.removeChild(div);
+    // }
 }
